2d724c421c6ef2f3fa1747bd94423f21136173f8
alumno <alumno@Instituto.local>
2025-05-21 08:23:46 -0300

    cambio

---
+++ clase1.js
@@ -0,0 +1,60 @@
+// Ejercicio 1: Crear y modificar un arreglo
+// 1
+let tresfrutas = ["Banana", "Manzana", "Naranja"]
+//2
+tresfrutas.push("Pera")
+console.log (tresfrutas [tresfrutas.length - 1])
+//3
+tresfrutas.unshift("Sandia")
+console.log (tresfrutas[0])
+//4
+colores.pop()
+console.log (tresfrutas)
+//5
+colores.shift()
+console.log (tresfrutas)
+ //Ejercicio 2: Contar elementos
+ let nombres = ["Marti", "Sasha", "Lourdes"]
+ //1
+let primero = nombres[0];
+//2
+console.log("Cuántos nombres hay en el arreglo" + nombres[nombres.length - 1])
+//3
+nombres.push("Camilo","Guido")
+console.log (nombres [nombres.length - 1])
+//4
+console.log ("Nueva cantidad de nombres", nombres.length)
+// Ejercicio 3: Recorrer un arreglo con for
+//1
+let numeros = [0,1,2,3,4]
+//2 (Usa un ciclo for para mostrar cada número por consola)
+    for (let indice = 0; index < numeros.legh ;indice ++) {
+    console.log (numeros [índice])
+    }
+// Ejercicio 4: Sumar elementos de un arreglo
+   //1
+   function sumarElementosArray(array)
+    {
+    let suma = 0; // Inicializar la variable para almacenar la suma
+    // Iterar sobre cada elemento del array
+    for (let i = 0; i < array.length; i++) {
+      suma += array[i]; // Sumar el elemento actual a la variable suma
+    }
+    return suma;
+  }
+  console.log("Resultado " + suma)
+ // Ejercicio 5: Cargar elementos con prompt()
+ 
+
+
+
+    
+
+
+
+
+
+
+
+
+
